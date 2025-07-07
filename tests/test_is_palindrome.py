671bd52322653d203f3867a574ceac4717700edb
B <beversprof25@outlook.com>
2025-07-07 09:10:50 +0100

    Refactor palindrome ceck function for conciseness

--- tests/test_is_palindrome.py
+++ tests/test_is_palindrome.py
@@ -4,9 +4,5 @@ def test_is_palindrome():
     assert is_palindrome("racecar") is True
 
 def is_palindrome(my_str):
-    if my_str == my_str[::-1]:
-        return True
-    else:
-        return False
-
+    return my_str == my_str[::-1]
 
